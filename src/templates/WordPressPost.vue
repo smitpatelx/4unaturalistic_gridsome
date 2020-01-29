2b7945ee7e5ab4f87ea0176e02c87ff07e07838b
Smit Patel <smitbpatel1999@gmail.com>
2020-01-29 17:43:33 -0500

    Design Fix

--- src/templates/WordPressPost.vue
+++ src/templates/WordPressPost.vue
@@ -26,11 +26,14 @@
                 <div class="w-full flex flex-wrap justify-center items-center font-normal text-base text-gray-700">
                     <span class="mr-2">
                         Posted <span class="font-semibold">{{$page.wordPressPost.dateGmt|formatDate}}</span> by</span>
-                    <g-image :src="$page.wordPressPost.author.avatarUrls._96" alt="author-avatar" class="bg-white shadow-md rounded-full h-12 w-12 ml-1 mr-3"/>
+                    <span class="font-semibold"> - {{avgReadTime}} min read</span>
+                </div>
+                <div class="w-full flex flex-wrap justify-center items-center font-normal text-base text-gray-700 self-auto my-4">
+                    <span class="mr-2">By</span> 
+                    <g-image :src="$page.wordPressPost.author.avatarUrls._96" alt="author-avatar" class="bg-white shadow-md rounded-full h-12 w-12 mx-3"/>
                     <g-link class="mr-2 text-teal-600 hover:text-gray-800 hover:underline" 
                         style="transition: all 0.5s;"
                         :href="`/author/${$page.wordPressPost.author.slug}`">{{$page.wordPressPost.author.name}}</g-link>
-                    <span class="font-semibold"> - {{avgReadTime}} min read</span>
                 </div>
                 <g-image class="rounded-lg my-6 shadow-lg object-cover w-full" style="max-height: 18rem;" quality="30" :src="$page.wordPressPost.featuredMedia.mediaDetails.sizes.mediumLarge.sourceUrl" :alt="$page.wordPressPost.featuredMedia.altText"/>
                 <div class="wp-content py-10 w-full" v-html="$page.wordPressPost.content"></div>
